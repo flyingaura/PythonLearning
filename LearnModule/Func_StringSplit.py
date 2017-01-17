67138acdc1b7df28778e9e1e585c336a762cdcb8
flyingaura <flyingaura@163.com>
2017-01-17 19:21:31 +0800

    Merge branch 'master' of https://git.oschina.net/flyingaura/learning2017
    # Please enter a commit message to explain why this merge is necessary,
    # especially if it merges an updated upstream into a topic branch.
    #
    # Lines starting with '#' will be ignored, and an empty message aborts
    # the commit.

--- LearnModule/Func_StringSplit.py
+++ LearnModule/Func_StringSplit.py
@@ -10,6 +10,8 @@ import functools
 def stringsplit(strN,splitN):
     # strN:待拆分字符串
     # splitN:分隔符,可以多个
+    if(not isinstance(strN,str)):
+        return None
     i = 0
     strL = []
     for n in range(len(strN)):
@@ -17,12 +19,13 @@ def stringsplit(strN,splitN):
             if(i != n):       # 抛掉空串
                 strL.append(strN[i:n])
             i = n+1
-    if(i < n):
+    if(i <= n):
         strL.append(strN[i:])
     return strL
-
-# stringOrigin = 'asdkfj,as23lwj,0129,ddd,1iasdfj,83484,sdkf,aaaddd,kfjdkj,'
-# stringSP = stringsplit(stringOrigin,['j',','])
+#
+# stringOrigin = '银行卡:7251,支付:4216,前绑定:3991,账户:2513,身份证:1845,姓名:1118,登录:1029,暂时:798,更改:724,ajf'
+# print(stringOrigin[2:])
+stringSP = stringsplit(None,['j',','])
 # print(stringSP)
 # print('the origin string is :',stringOrigin)
 # for n in stringSP:
