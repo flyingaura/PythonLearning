6554f4a28edb92c522f4fecc9eb5e3accd14d9f1
flyingaura <flyingaura@163.com>
2017-01-20 17:54:17 +0800

    Merge branch 'master' of https://git.oschina.net/flyingaura/learning2017
    # Please enter a commit message to explain why this merge is necessary,
    # especially if it merges an updated upstream into a topic branch.
    #
    # Lines starting with '#' will be ignored, and an empty message aborts
    # the commit.

--- LearnModule/Func_StringSplit.py
+++ LearnModule/Func_StringSplit.py
@@ -5,12 +5,34 @@ __author__ = 'Flyingaura_wl'
 __edittime__ = '20170101'
 
 import math
+import time
 import functools
 
+# 增加日志输出功能
+def log(func):
+    def wrapper(*args,**kw):
+        with open('C:/Users/flyingaura/Desktop/stringsplit_log.txt', 'w') as logfile:
+            logfile.write('functions %s start at ' %func.__name__)
+            logfile.write(time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())))
+            logfile.write('\n')
+            ret = func(*args,**kw)
+            if(not isinstance(args[0],str)):
+                logfile.write('ERROR!! The first parameter of %s type is wrong! \n' %func.__name__)
+            elif(args[0] == ''):
+                logfile.write('ERROR! The first parameter of %s should not be Null \n' %func.__name__)
+            else:
+                logfile.write('Success! \n')
+            logfile.write('functions end at ')
+            logfile.write(time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())))
+            logfile.write('\n')
+        return ret
+    return wrapper
+
+@log
 def stringsplit(strN,splitN):
     # strN:待拆分字符串
     # splitN:分隔符,可以多个
-    if(not isinstance(strN,str)):
+    if(not isinstance(strN,str) or strN == ''):
         return None
     i = 0
     strL = []
@@ -25,8 +47,8 @@ def stringsplit(strN,splitN):
 #
 # stringOrigin = '银行卡:7251,支付:4216,前绑定:3991,账户:2513,身份证:1845,姓名:1118,登录:1029,暂时:798,更改:724,ajf'
 # print(stringOrigin[2:])
-stringSP = stringsplit(None,['j',','])
-# print(stringSP)
+# stringSP = stringsplit(stringOrigin,['j',','])
+# print(stringsplit('',['j',',']))
 # print('the origin string is :',stringOrigin)
 # for n in stringSP:
 #     print(n)
