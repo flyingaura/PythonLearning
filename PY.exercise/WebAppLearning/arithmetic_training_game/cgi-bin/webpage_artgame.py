e61a0ae572716b466e4253fcad75391bf2e62507
flyingaura <flyingaura@163.com>
2017-11-13 23:01:22 +0800

    Merge branch 'master' of https://git.oschina.net/flyingaura/learning2017
    # Conflicts:
    #	.idea/workspace.xml

--- PY.exercise/WebAppLearning/arithmetic_training_game/cgi-bin/webpage_artgame.py
+++ PY.exercise/WebAppLearning/arithmetic_training_game/cgi-bin/webpage_artgame.py
@@ -10,13 +10,13 @@ print(yate.include_header('欢迎来到韦浩宇的算术运算训练营！'))
 print(yate.para('即将开始算术训练！'))
 
 Numlist = [x for x in range(20)]
-ArtInstance = arithmetic_training_games.ArithmeticMode(Numlist,level= 3, OperatorList=['+','-'])
+ArtInstance = arithmetic_training_games.ArithmeticMode(Numlist,level= 2, OperatorList=['-'])
 
 while(True):
     AnExpress = ArtInstance.get_ArtExpress()
     if(0 <= eval(AnExpress) <= 100):
         break
-print(yate.header('%s = ' %(AnExpress), header_level = 2))
+print(yate.header('%s = ' %(AnExpress), header_level = 1))
 print(yate.header('更多算题，敬请期待！'))
 
 # print('%s = ' %(AnExpress))
