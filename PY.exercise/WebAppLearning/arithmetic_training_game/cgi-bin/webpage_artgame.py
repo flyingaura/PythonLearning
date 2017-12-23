142efb8acd4c691fd2eaf35ee1fdf82b96df63e0
flyingaura <flyingaura@163.com>
2017-12-23 14:34:23 +0800

    Merge branch 'master' of https://git.oschina.net/flyingaura/learning2017
    # Conflicts:
    #	.idea/workspace.xml

--- PY.exercise/WebAppLearning/arithmetic_training_game/cgi-bin/webpage_artgame.py
+++ PY.exercise/WebAppLearning/arithmetic_training_game/cgi-bin/webpage_artgame.py
@@ -7,8 +7,8 @@ import yate,arithmetic_training_games
 print(yate.start_response())
 print(yate.include_header('欢迎来到韦浩宇的算术运算训练营！'))
 # print(yate.start_form('arithmetic_training_games.py'))
-print(yate.para('即将开始算术训练！'))
-
+print(yate.para('即将开始算术训练！',fontsize = 15,fontcolor = '#a52a2a'))
+print(yate.start_form(''))
 Numlist = [x for x in range(20)]
 ArtInstance = arithmetic_training_games.ArithmeticMode(Numlist,level= 2, OperatorList=['-'])
 
@@ -16,7 +16,10 @@ while(True):
     AnExpress = ArtInstance.get_ArtExpress()
     if(0 <= eval(AnExpress) <= 100):
         break
-print(yate.header('%s = ' %(AnExpress), header_level = 1))
+print(yate.header('%s = %s' %(AnExpress,yate.text_inputs('ArithmeticValue') + yate.submit_button('确定')), header_level = 1))
+# print()
+print(yate.end_form())
+
 print(yate.header('更多算题，敬请期待！'))
 
 # print('%s = ' %(AnExpress))
--- PY.exercise/WebAppLearning/arithmetic_training_game/cgi-bin/yate.py
+++ PY.exercise/WebAppLearning/arithmetic_training_game/cgi-bin/yate.py
@@ -22,16 +22,18 @@ def include_footer(the_links):
 def start_form(the_url, form_type="POST"):
     return('<form action="' + the_url + '" method="' + form_type + '">')
 
-def end_form(submit_msg="Submit"):
-    return('<p></p><input type=submit value="' + submit_msg + '"></form>')
+def end_form():
+    return('</form>')
 
 def radio_button(rb_name, rb_value):
     return('<input type="radio" name="' + rb_name +
                              '" value="' + rb_value + '"> ' + rb_value + '<br />')
 
-# def create_inputs(inputs_str):
-#     html_inputstr = '<input type = "text" name = "' +
+def text_inputs(Tname):
+    return('<input type = "text" name = "' + Tname + '"> &nbsp;&nbsp;&nbsp;')
 
+def submit_button(submit_msg="Submit"):
+    return('<input type="submit" value="' + str(submit_msg) + '">')
 
 def u_list(items):
     u_string = '<ul>'
@@ -44,7 +46,7 @@ def header(header_text, header_level=2):
     return('<h' + str(header_level) + '>' + header_text +
            '</h' + str(header_level) + '>')
 
-def para(para_text):
-    return('<p>' + para_text + '</p>')
+def para(para_text,fontsize = 13,fontcolor = 'black'):
+    return('<p><font size="' + str(fontsize) + '"' + ' color="' + fontcolor + '"' + '>' + para_text + '</font></p>')
 
 
